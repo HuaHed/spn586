dc8f5e2c6df726bf47f91d654483c7f1a9758cc2
HuaHed <124671126+HuaHed@users.noreply.github.com>
2023-04-25 15:39:31 +0700

    Add files via upload

---
+++ tourpoint.js
@@ -0,0 +1,125 @@
+var tourpoint = {
+  "type": "FeatureCollection",
+  "features": [
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "วัดพระมหาธาตุวรมหาวิหาร",
+        "type": "Culture",
+        "level": 7
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [100.575590, 14.019996]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "สวนน้ำริมน้ำ",
+        "type": "Natural",
+        "level": 3
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [100.609665, 14.013111]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "วัดบางกระเจ้า",
+        "type": "Culture",
+        "level": 6
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [100.540811, 14.017434]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "เกาะเสม็ด",
+        "type": "Natural",
+        "level": 5
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [100.599289, 14.045212]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "อุทยานประวัติศาสตร์ราชพฤกษ์",
+        "type": "Special",
+        "level": 9
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [100.574954, 14.062722]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "วัดชัยมงคล",
+        "type": "Culture",
+        "level": 4
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [100.531068, 14.023402]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "สวนสัตว์เปิดไทย",
+        "type": "Special",
+        "level": 8
+      },
+      "geometry": {
+      "type": "Point",
+      "coordinates": [100.623154, 14.079066]
+      }
+      },
+      {
+      "type": "Feature",
+      "properties": {
+      "name": "วัดหลวงพ่อโสธร",
+      "type": "Culture",
+      "level": 10
+      },
+      "geometry": {
+      "type": "Point",
+      "coordinates": [100.498893, 14.003954]
+      }
+      },
+      {
+      "type": "Feature",
+      "properties": {
+      "name": "บ้านสำราญ",
+      "type": "Special",
+      "level": 2
+      },
+      "geometry": {
+      "type": "Point",
+      "coordinates": [100.562301, 14.036275]
+      }
+      },
+      {
+      "type": "Feature",
+      "properties": {
+      "name": "สวนสาธารณะบางกระเจ้า",
+      "type": "Natural",
+      "level": 1
+      },
+      "geometry": {
+      "type": "Point",
+      "coordinates": [100.541057, 14.041184]
+      }
+      }
+      ]
+      }
